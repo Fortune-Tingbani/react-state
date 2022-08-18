2d2970ffe3e82980d64452e3b75a503430192636
Fortune-Tingbani <fortunetingbani@gmail.com>
2022-08-18 11:30:47 -0700

    commit 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import ProfileForm from './component/ProfileForm'
 import ProfileCards from './component/ProfileCards'
+import { useState } from 'react'
 
 function App() {
+  const [allProfiles, setAllprofiles] = useState([
+
+    {
+      firstname:"Fortunate",
+      lastnmae:"Tingbani",
+      email:"fortunetingbani@gmail.com",
+      phone:"0249961983"
+    }
+  ])
+
+  const updateallprofiles = (profile) => {
+    let arr = allProfiles;
+    arr.push(profile)
+    setAllprofiles([...arr])
+  };
+
+
+
   return (
-    <div>App</div>
+    <>
+    <div className="app">
+    <h1>Our Profile maker</h1>
+    </div>
+    <ProfileForm submit={updateallprofiles} />
+    <hr />
+    <div className="list">
+    {allProfiles.map( (person, index) => (
+      <ProfileCards key={index} card={person} />
+      ))}
+    
+    </div>
+    </>
   )
 }
 
--- src/component/ProfileForm.js
+++ src/component/ProfileForm.js
@@ -1,71 +1,62 @@
 import React from 'react'
 import { useState } from 'react'
 
-function ProfileForm() {
+function ProfileForm({ submit }) {
     const [profiles, setProfiles] = useState({
-        firstname:"",
-        lastname:"",
-        email:"",
-        phone:""
+        firstname: "",
+        lastname: "",
+        email: "",
+        phone: ""
     })
 
     const handler = (event) => {
         setProfiles((prev) => ({
             ...prev,
-            [event.target.name]:event.target.value 
+            [event.target.name]: event.target.value
         }))
     }
 
     const handleForm = (e) => {
         e.preventDefault();
-        Submit(profile)
+        submit(profiles)
     }
 
 
 
-  return (
-    <div className="formContainer">
-    <h3>profile Form</h3>
-    <form>
-    <fieldset>
-    <legend>Bio data</legend>
-    <div className="names">
-    <label>first name</label>
-    <input name="firstname" value={profile.firstname} 
-    type="text" onChange={handler}/>
-   
-    <label>last name</label>
-    <input name="lastname" value={profile.firstname} 
-    type="text" onChange={handler}/>
-    
-    </div>
-    <legend>Bio data</legend>
-    <div className="names">
-    <label>first name</label>
-    <input name="firstname" value={profile.firstname} 
-    type="text" onChange={handler}/>
-   
-    <label>last name</label>
-    <input name="lastname" value={profile.firstname} 
-    type="text" onChange={handler}/>
-    
-    </div>
+    return (
+        <div className="formContainer">
+            <h3>profile Form</h3>
+            <form onSubmit={handleForm}>
+                <fieldset>
+                 
+                    <legend>Bio data</legend>
+                    <div className="names">
+                        <label>first name</label>
+                        <input name="firstname" value={profiles.firstname}
+                            type="text" onChange={handler} />
 
-    <div className="names">
-    <label>email</label>
-    <input name="email" value={profile.firstname} 
-    type="text" onChange={handler}/>
-   
-    <label>phone</label>
-    <input name="phone" value={profile.firstname} 
-    type="text" onChange={handler}/>
-    
-    </div>
-    </fieldset>
-    </form>
-    
-    </div>
-  )
+                        <label>last name</label>
+                        <input name="lastname" value={profiles.lastname}
+                            type="text" onChange={handler} />
+
+                    </div>
+
+                    <div className="names">
+                        <label>email</label>
+                        <input name="email" value={profiles.email}
+                            type="text" onChange={handler} />
+
+                        <label>phone</label>
+                        <input name="phone" value={profiles.phone}
+                            type="text" onChange={handler} />
+
+                    </div>
+                </fieldset>
+                <button className='form' type="submit">Add Profile</button>
+            </form>
+
+        </div>
+    )
 }
 
 export default ProfileForm
